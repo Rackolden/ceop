6c1f72e34372f07dc9081b05639294957906758a
Rackolden <frank10cordova@hotmail.com>
2024-09-18 16:55:50 -0500

    antes de irme por las ramas xd

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -26,55 +26,56 @@ function NavBar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-Logo">
-            CEOP
+            <h1 className="Title">CEOP</h1>&nbsp;
+            <h2 className="Title-secondary">Business</h2>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fa-solid fa-x" : "fa-solid fa-bars"}></i>
           </div>
-            <ul className={click ? "nav-menu active" : "nav-menu"}>
-              <li className="nav-item">
-                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                  INICIO
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/nosotros"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  NOSOTROS
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/servicios"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  SERVICIOS
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/contacto"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  CONTACTO
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/recursos"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  RECURSOS
-                </Link>
-              </li>
-            </ul>
-            {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                INICIO
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/nosotros"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                NOSOTROS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/servicios"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                SERVICIOS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contacto"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                CONTACTO
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/recursos"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                RECURSOS
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">INGRESAR</Button>}
         </div>
       </nav>
     </>
